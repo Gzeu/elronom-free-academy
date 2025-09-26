e8d4b93ee767e57678c41e072b359d2ace5083d2
George Pricop <96602369+Gzeu@users.noreply.github.com>
2025-09-26 13:02:01 +0300

    🏗️ Complete project structure: React app, Supabase schema, CI/CD, and deployment config

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Header } from './components/Header'
-import { Footer } from './components/Footer'
-import { HomePage } from './pages/HomePage'
-import { LessonsPage } from './pages/LessonsPage'
-import { CommunityPage } from './pages/CommunityPage'
-import { WalletPage } from './pages/WalletPage'
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Lessons from "./pages/Lessons";
+import Wallet from "./pages/Wallet";
+import Community from "./pages/Community";
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex flex-col">
-        <Header />
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/lessons" element={<LessonsPage />} />
-            <Route path="/community" element={<CommunityPage />} />
-            <Route path="/wallet" element={<WalletPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  )
-}
-
-export default App
+    <div className="min-h-screen": 
